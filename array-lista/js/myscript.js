23cae6e01986569cce004f60246962f5b6b94f16
Nik0-9 <filannino.nicola9@gmail.com>
2024-03-12 14:05:28 +0100

    fix snack 1

--- array-lista/js/myscript.js
+++ array-lista/js/myscript.js
@@ -5,30 +5,36 @@ C'è anche una pesca sul tavolo, la mettiamo nel frigo.
 Stasera dobbiamo fare un cocktail a base di cocomero: ce l'abbiamo o no in frigo?
    - se c'è stampiamo: "Trovato! Devo solo preparare il cocktail."
    - se non lo trovo: "Oh no, devo uscire a comprare il cocomero!"
-   */
+*/
 
-let frigorifero = [
-    'banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola'
-];
-
-
-let addFruit = 'pesca';
-frigorifero.push(addFruit);
-console.log(frigorifero);
-exist = false;
-let input = document.getElementById('frutta').value;
 let btn = document.querySelector('.btn.btn-primary');
 
 btn.addEventListener('click', function(){
-    for(i=0; i<=frigorifero.length - 1; i++){
-        if(input.toLowerCase === frigorifero[i].toLowerCase){
+    let frigorifero = [
+        'banana',
+        'mela',
+        'pera',
+        'ciliegia',
+        'arancia',
+        'mandarino',
+        'cocomero',
+        'limone',
+        'fragola'
+    ];
+    let addFruit = 'pesca';
+    frigorifero.push(addFruit);
+    console.log(frigorifero);
+    exist = false;
+    let stampa = document.getElementById('output');
+    let input = document.getElementById('frutta').value;
+    for(i = 0; i <= frigorifero.length - 1; i++){
+        if(input === frigorifero[i]){
             exist = true;
         }
     } if(exist){
-        let stampa = document.getElementById('output');
         stampa.innerHTML = 'Trovato! Devo solo preparare il cocktail.'
     } else{
-        stampa.innerHTML = 'se non lo trovo: "Oh no, devo uscire a comprare il cocomero!'
+        stampa.innerHTML = 'Oh no, devo uscire a comprare il cocomero!'
     }
 })
 
