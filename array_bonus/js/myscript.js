77d4c55557150ac85a1f9bd3c8313b2769fe5ee3
Nik0-9 <filannino.nicola9@gmail.com>
2024-03-13 13:16:12 +0100

    add input for create array and console log snack complete

--- array_bonus/js/myscript.js
+++ array_bonus/js/myscript.js
@@ -3,10 +3,38 @@ Crea due array che hanno un numero di elementi diversi.
 Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
 */
 
-let ar1 = [10, 20, 30, 40, 50];
-let ar2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-let diff = 0;
-console.log(ar1, ar2);
+let btn = document.querySelector('.btn.btn-primary')
+
+btn.addEventListener('click', function(){
+    let ar1scan = document.getElementById('array1').value;
+    let ar2scan = document.getElementById('array2').value;
+    let ar1 = [];
+    let ar2 = [];
+    let diff = 0;
+    console.log(ar1scan, ar2scan);
+
+for(i=0; i<=ar1scan; i++){
+    ar1.push(getRndInteger(1,90));
+    for(i=0; i<ar1.length; i++){
+        if(ar1[i] === ar1[i - 1]){
+            // console.log(ar1[i], ar1[i - 1]);
+            ar1.pop();
+        }
+    }
+}
+for(i=0; i<=ar2scan; i++){
+    ar2.push(getRndInteger(1,90));
+    for(i=0; i<ar2.length; i++){
+        if(ar2[i] === ar2[i - 1]){
+            //console.log(ar1[i], ar1[i - 1]);
+            ar2.pop();
+        }
+    }
+}
+console.log('array1',ar1);
+console.log('array2',ar2);
+
+
 if (ar1.length < ar2.length){
     diff = parseInt(ar2.length - ar1.length);
 }else{
@@ -16,13 +44,17 @@ let add = 0;
 if(ar1.length !== ar2.length){
     for(i=0; i < diff; i++){
         if(ar1.length > ar2.length){
-            ar2.push(getRndInteger(1,99));
+            ar2.push(getRndInteger(91,99));
         } else{
-            ar1.push(getRndInteger(1,99));
+            ar1.push(getRndInteger(91,99));
         }
     }
 }else{
+
     console.log('Gli array hanno lo stesso numero di elementi');
 }
-console.log(diff);
-console.log(ar1, ar2);
+
+console.log('differenza di elementi',diff);
+console.log('array1',ar1);
+console.log('array2',ar2);
+})
